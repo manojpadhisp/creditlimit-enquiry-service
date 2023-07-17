f2980ddf4f7d201785140a4725bb7f32d165d84b
Manoj <manojpadhisp@gmail.com>
2023-07-17 17:26:45 +0530

    skelton project

---
+++ src/test/java/com/tesco/enquiry/CreditlimitEnquiryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesco.enquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditlimitEnquiryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c22833391e01459597b258eec4d2e67107a9b448
Manoj <manojpadhisp@gmail.com>
2023-08-01 22:30:02 +0530

    swagger document imp

--- src/main/java/com/tesco/enquiry/config/CreditLimitEnquirySwaggerConfig.java
+++ src/main/java/com/tesco/enquiry/config/CreditLimitEnquirySwaggerConfig.java
@@ -3,9 +3,45 @@
  */
 package com.tesco.enquiry.config;
 
+
+
+
+
+
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 /**
  * @Author Manoj by 17-Jul-2023
  */
+
 public class CreditLimitEnquirySwaggerConfig {
+	
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.tesco.enquiry.controller"))
+                .paths(PathSelectors.regex("/v1/*"))
+                .build().apiInfo(apiInfoMetaData());
+    }
+
+    private ApiInfo apiInfoMetaData() {
+
+        return new ApiInfoBuilder().title("Enrollment Service")
+                .description("API Endpoint Decoration")
+                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
 
 }
